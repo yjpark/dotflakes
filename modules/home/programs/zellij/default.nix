a43580a6f501e17837aaf15c14dff386af06ee1a
YJ Park <yjpark@gmail.com>
2025-10-05 16:02:21 +0800

    improve zellij settings

--- modules/home/programs/zellij/default.nix
+++ modules/home/programs/zellij/default.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }: {
+  imports =
+    with builtins;
+    map
+      (fn: ./${fn})
+      (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+
   programs.zellij = {
     enable = true;
     # This will automatically start zellij for new shell, which make working with remote machine much harder
     enableFishIntegration = false;
   };
-  # https://github.com/nix-community/home-manager/pull/4465
-  # it's quite tricky to config mappings with home-manager
-  home.file.".config/zellij/config.kdl".source = ./config.kdl;
-  home.file.".config/zellij/layouts/zjstatus.kdl".source = ./layouts/zjstatus.kdl;
-  home.file.".config/zellij/plugins/zjstatus.wasm".source = ./plugins/zjstatus.wasm;
 }
---
+++ modules/home/programs/zellij/plugins/default.nix
@@ -0,0 +1,16 @@
+
+{ lib, pkgs, ... }:
+let
+  targetDir = ./.;
+  dirContents = builtins.readDir targetDir;
+  files = builtins.filter (name: name != "default.nix") (builtins.attrNames dirContents);
+  withSources = builtins.listToAttrs (builtins.map (name: {
+    name = ".config/zellij/plugins/" + name;
+    value = {
+      source = (targetDir + "/${name}");
+    };
+  }) files);
+in
+{
+  home.file = withSources;
+}
