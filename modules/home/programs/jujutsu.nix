0677104af82adde6d5c40bdde28c37d6c0afe439
YJ Park <yjpark@gmail.com>
2025-10-10 22:59:57 +0800

    bugfix with jj config

--- modules/home/programs/jujutsu.nix
+++ modules/home/programs/jujutsu.nix
@@ -1,10 +1,12 @@
 { config, ... }: {
-  programs.jujutsu.enable = true;
-  programs.jjui = {
+  programs.jujutsu = {
     enable = true;
     settings = {
       email = config.me.email;
       name = config.me.fullname;
     };
+  };
+  programs.jjui = {
+    enable = true;
   };
 }
