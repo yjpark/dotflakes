aa5c1f7d41f1be4872dea15a136eecce30c5b07b
YJ Park <yjpark@gmail.com>
2025-10-11 14:15:24 +0800

    Try using Droidcam

---
+++ modules/home/programs/obs.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      droidcam-obs
+    ];
+  };
+  home.packages = with pkgs; [
+    droidcam
+  ];
+}
