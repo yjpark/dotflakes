6fb1403178360a3af750ac8bed8df8195288fba3
YJ Park <yjpark@gmail.com>
2025-10-11 17:25:18 +0800

    Update with flakelight, folder adjust

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    flakelight.url = "github:nix-community/flakelight";
+    flakelight.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-unified.url = "github:srid/nixos-unified";
 
@@ -26,6 +29,9 @@
 
   # Wired using https://nixos-unified.org/autowiring.html
   outputs = inputs:
-    inputs.nixos-unified.lib.mkFlake
-      { inherit inputs; root = ./.; };
+    inputs.flakelight ./. {
+      nixDirAliases.homeModules = [ "home" ];
+    }
+    #inputs.nixos-unified.lib.mkFlake
+    #  { inherit inputs; root = ./.; };
 }
