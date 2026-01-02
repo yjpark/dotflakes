23467105aa52eb60e433e953d85526f0e9add2fd
YJ Park <yjpark@gmail.com>
2026-01-02 21:43:41 +0800

    cleanup niri

--- modules/home/files/linux/default.nix
+++ modules/home/files/linux/default.nix
@@ -1,5 +1,3 @@
 {
   home.file.".config/run-or-raise/shortcuts.conf".source = ./run-or-raise.shortcuts.conf;
-  home.file.".config/niri/config.kdl".source = ./niri.config.kdl;
-  home.file.".config/niri/justfile".source = ./niri.justfile;
 }
--- modules/home/files/linux/niri.justfile
+++
@@ -1,7 +0,0 @@
-backup-to-flakes:
-  cp -v config.kdl ~/.flakes/modules/home/files/linux/niri.config.kdl
-
-switch-to-local:
-  rm -v config.kdl
-  cp -v ~/.flakes/modules/home/files/linux/niri.config.kdl config.kdl
-  nvim config.kdl
