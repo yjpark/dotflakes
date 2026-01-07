fcd995a6c0d8438b461fb5223e00d31bf4773f1e
YJ Park <yjpark@gmail.com>
2026-01-07 10:34:35 +0800

    add flox cache

---
+++ modules/nixos/settings/nix/flox.nix
@@ -0,0 +1,4 @@
+{
+  nix.settings.trusted-substituters = [ "https://cache.flox.dev" ];
+  nix.settings.trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
+}
