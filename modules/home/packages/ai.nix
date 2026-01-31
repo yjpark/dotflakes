9a12891cddebc52edb160498616d6b3a204bf347
YJ Park <yjpark@gmail.com>
2026-01-31 14:29:28 +0800

    update scripts

--- modules/home/packages/ai.nix
+++ modules/home/packages/ai.nix
@@ -1,9 +1,14 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    goose-cli
     nodejs_25
-    (pkgs.writeShellScriptBin "ai-install-gemini-cli"  ''
+    (pkgs.writeShellScriptBin "gemini"  ''
       #!/usr/bin/env bash
-      npm install -g @google/gemini-cli@latest
+      npx @google/gemini-cli@latest "$@"
+    '')
+    (pkgs.writeShellScriptBin "mcpjam"  ''
+      #!/usr/bin/env bash
+      npx @mcpjam/inspector@latest "$@"
     '')
   ];
 }
