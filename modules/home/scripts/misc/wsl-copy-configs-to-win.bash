1a2195b6d73d468dd21ce6abfe9a8ee9fd87458b
YJ Park <yjpark@gmail.com>
2026-03-01 01:05:43 +0800

    copy vscode and antigravity config from wsl

--- modules/home/scripts/misc/wsl-copy-configs-to-win.bash
+++ modules/home/scripts/misc/wsl-copy-configs-to-win.bash
@@ -3,3 +3,7 @@
 cp -v ~/.config/wezterm/* ~/win/.config/wezterm/
 
 cp -v ~/.config/zed/* ~/win/AppData/Roaming/Zed/
+
+cp -v ~/.config/Code/User/*.json ~/win/AppData/Roaming/Code/User/
+
+cp -v ~/.config/Code/User/*.json ~/win/AppData/Roaming/Antigravity/User/
