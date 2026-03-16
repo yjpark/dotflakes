720d4d6ca18c949b5a3d372733d81de5a329842f
YJ Park <yjpark@gmail.com>
2026-03-16 23:56:53 +0800

    more cleanup

--- modules/nixos/settings/console.nix
+++ modules/nixos/settings/console.nix
@@ -1,5 +1,4 @@
 {
   console.font = "latarcyrheb-sun32";
   #console.keyMap = "dvorak";
-  console.useXkbConfig = true;
 }
--- modules/nixos/settings/locale.nix
+++ modules/nixos/settings/locale.nix
@@ -1,6 +1,4 @@
 {
-  time.timeZone = "Asia/Shanghai";
-
   i18n = {
     defaultLocale = "en_US.UTF-8";
   };
