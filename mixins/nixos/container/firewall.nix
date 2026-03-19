f25a971ac05ae6b7abe2d93fb61bc61b962a53f4
YJ <yj@example.com>
2026-03-19 16:00:22 +0000

    move beans folder, fix firewall

--- mixins/nixos/container/firewall.nix
+++ mixins/nixos/container/firewall.nix
@@ -1,5 +1,7 @@
 {...}: {
+  services.firewalld.settings.DefaultZone = "lan";
   services.firewalld.zones.lan = {
+    interfaces = [ "eth0" ];
     sources = [ { address = "10.0.0.0/8"; } ];
     ports = [
       { port = { from = 1000; to = 65535; }; protocol = "tcp"; }
