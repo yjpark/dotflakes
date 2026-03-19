e7728fdb1a03942b17da0abd59384ba580699ee3
YJ <yj@example.com>
2026-03-19 15:24:44 +0000

    cleanup nix-ld

--- mixins/nixos/container/nix-ld.nix
+++ mixins/nixos/container/nix-ld.nix
@@ -1,11 +1,10 @@
 {pkgs, ...}: {
-  # Enable nix ld
+  # Enable nix-ld to allow running dynamically linked binaries (e.g. npm-installed CLIs)
   programs.nix-ld.enable = true;
 
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc
     zlib
-    fuse3
     openssl
   ];
 }
