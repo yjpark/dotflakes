8c5a0f80a09f5c05474d199592c24e0857fe4d68
YJ Park <yjpark@gmail.com>
2026-03-23 02:46:35 +0000

    bugfixes

--- mixins/home/ai/llm-agents.nix
+++ mixins/home/ai/llm-agents.nix
@@ -4,9 +4,6 @@
   ...
 }:
 {
-  imports = [
-    (flake.inputs.self + /mixins/home/host/common/ai.nix)
-  ];
   programs.claude-code.package = flake.inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-code;
 
   home.packages = with flake.inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
--- mixins/home/trails/ai.nix
+++ mixins/home/trails/ai.nix
@@ -1,41 +1,22 @@
 {
-  flake,
   pkgs,
   ...
 }:
-let
-    dolt-version = "1.83.5";
-    dolt = pkgs.dolt.overrideAttrs (old: {
-      version = dolt-version;
-      src = pkgs.fetchFromGitHub {
-        owner = "dolthub";
-        repo = "dolt";
-        tag = "v${dolt-version}";
-        hash = "sha256-UaC9Yl3xl3IWQN7RSu1ApJNgm/fIgvLgoxOFWEVJK28=";
-      };
-      vendorHash = "sha256-hnJhLEJo/EQlTuTv+smiLok7AarFoDIB4ebB6ncUYtc=";
-      doCheck = false;
-    });
-in
 {
-  home.packages = with flake.inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
-    ck
-    beads
-    dolt
-
-    (pkgs.writeShellScriptBin "install-googleworkspace-cli"  ''
+  home.packages = with pkgs; [
+    (writeShellScriptBin "install-googleworkspace-cli"  ''
       #!/usr/bin/env bash
 
       npm install -g @googleworkspace/cli
     '')
 
-    (pkgs.writeShellScriptBin "install-claude-trace"  ''
+    (writeShellScriptBin "install-claude-trace"  ''
       #!/usr/bin/env bash
 
       npm install -g @mariozechner/claude-trace
     '')
 
-    (pkgs.writeShellScriptBin "mcpjam"  ''
+    (writeShellScriptBin "mcpjam"  ''
       #!/usr/bin/env bash
       npx @mcpjam/inspector@latest "$@"
     '')
