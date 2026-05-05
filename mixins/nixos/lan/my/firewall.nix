c157db06d2b7cffaa54da4bdefc27102f644015d
YJ Park <yjpark@gmail.com>
2026-05-05 14:10:24 +0800

    update firewall rules

--- mixins/nixos/lan/my/firewall.nix
+++ mixins/nixos/lan/my/firewall.nix
@@ -3,8 +3,11 @@
     sources = [ { address = "10.0.0.0/16"; } { address = "172.22.0.0/16"; } ];
     protocols = [ "icmp" ];
     ports = [
-      { port = { from = 1000; to = 65535; }; protocol = "tcp"; }
-      { port = { from = 1000; to = 65535; }; protocol = "udp"; }
+      { port = 22; protocol = "tcp"; }
+      { port = 80; protocol = "tcp"; }
+      { port = 443; protocol = "tcp"; }
+      { port = { from = 1; to = 65535; }; protocol = "tcp"; }
+      { port = { from = 1; to = 65535; }; protocol = "udp"; }
     ];
   };
 }
--- packs/nixos/container/firewall.nix
+++ packs/nixos/container/firewall.nix
@@ -5,6 +5,7 @@
     sources = [ { address = "10.0.0.0/16"; } { address = "172.22.0.0/16"; } ];
     protocols = [ "icmp" ];
     ports = [
+      { port = 22; protocol = "tcp"; }
       { port = 80; protocol = "tcp"; }
       { port = 443; protocol = "tcp"; }
       { port = { from = 1000; to = 65535; }; protocol = "tcp"; }
