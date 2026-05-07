80cf6c31f65ee8b8dd17750f28157363e88b69fb
YJ Park <yjpark@gmail.com>
2026-05-07 14:43:45 +0800

    update claude settings

--- packs/home/common/programs/claude/settings.nix
+++ packs/home/common/programs/claude/settings.nix
@@ -3,6 +3,8 @@
 }: {
   home.sessionVariables = {
     CLAUDE_CODE_NEW_INIT = "1";
+    CLAUDE_CODE_NO_FLICKER = "1";
+    #CLAUDE_CODE_DISABLE_ALTERNATE_SCREEN = "1";
   };
 
   programs.claude-code = {
