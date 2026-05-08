e453104abb5c544850a92a28d8ccde2307480de0
YJ Park <yjpark@gmail.com>
2026-05-09 00:21:30 +0800

    tweak script

--- packs/home/host/common/scripts/nixos/misc/copy-k3s-yaml.bash
+++ packs/home/host/common/scripts/nixos/misc/copy-k3s-yaml.bash
@@ -2,5 +2,5 @@
 
 sudo cp /etc/rancher/k3s/k3s.yaml .secret.k3s.yaml
 sudo chown ${USER}:users .secret.k3s.yaml
-sudo chmod 400 .secret.k3s.yaml
+sudo chmod 600 .secret.k3s.yaml
 cp .secret.k3s.yaml ~/.kube/config
